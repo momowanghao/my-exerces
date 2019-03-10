37c59a161363bc7c1488e67e2b3f042117c8ba46
wanghao <13890733206@163.com>
2019-03-10 17:36:43 +0800

    pos/vue的项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <leftNav></leftNav>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'App',
+  components:{
+    leftNav
+  }
+}
+</script>
+<style>
+</style>
